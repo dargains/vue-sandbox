f04030b2b397dedc2845433050c92d6afa1daab4
Andre Dargains <andre.dargains@gmail.com>
2018-03-06 12:09:01 +0000

    UPD: routing;

---
+++ src/Product.vue
@@ -0,0 +1,37 @@
+<template lang="html">
+  <div>
+    <router-link to="/products">Back to all</router-link>
+    <h2><strong>{{product.name}}</strong></h2>
+    <img :src="product.image">
+    <p>{{product.description}}</p>
+    <strong>Price: ${{product.price}}</strong>
+    <div class="">
+      <button type="button" name="button">Add to cart</button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "product",
+  data() {
+    return {
+      product: {}
+    }
+  },
+  created() {
+    let id = this.$route.params.id;
+    $.getJSON(`https://hplussport.com/api/products/id/${id}`)
+    .done(data => {
+      this.product = data[0];
+    })
+  }
+}
+</script>
+
+<style lang="css" scoped>
+  img {
+    width: 700px;
+    height: 700px;
+  }
+</style>
---
+++ src/ProductListItem.vue
@@ -0,0 +1,30 @@
+<template lang="html">
+  <li>
+    <img :src="product.image">
+    <p><strong><router-link :to="`/products/${product.id}`">{{product.name}}</router-link></strong></p>
+    <p>{{product.description}} <a @click="requestRemoval">Hide this item</a></p>
+  </li>
+</template>
+
+<script>
+export default {
+  name: "product-list-item",
+  props: ['product'],
+  methods: {
+    requestRemoval() {
+      this.$emit('remove');
+    }
+  }
+}
+</script>
+
+<style lang="css" scoped>
+  img {
+    float: left;
+    width: 300px;
+  }
+  li {
+    margin-bottom: 40px;
+    clear: both;
+  }
+</style>
